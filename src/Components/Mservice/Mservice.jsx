66120cdbaab914b61a34973794ae10a37e942090
kdrbipul <kdrbipul@gmail.com>
2023-10-04 13:25:15 +0600

    last commit

---
+++ src/Components/Mservice/Mservice.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mservice = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Mservice;
---
+++ src/Route/Router.jsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Components/Home/Home";
+import Main from "../Layout/Main";
+import Service from './../Components/Services/Service';
+import Mservice from './../Components/Mservice/Mservice';
+import Project from "../Components/Project/Project";
+import SignIn from "../UserLogin/SignIn/SignIn";
+import Contact from "../Components/Contact/Contact";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main />,
+        children:[
+            {
+                path:'/',
+                element:<Home />
+            },
+            {
+                path:'/service',
+                element:<Service />
+            },
+            {
+                path:'/mservice',
+                element:<Mservice />
+            },
+            {
+                path:'/project',
+                element:<Project />
+            },
+            {
+                path:'/contact',
+                element:<Contact />
+            },
+            {
+                path:'/signin',
+                element:<SignIn />
+            }
+        ]
+    }
+])
+
+export default router;
