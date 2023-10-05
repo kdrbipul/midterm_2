21ee64ddcfcc58fe9f82e26302b2c45f9fa05a35
kdrbipul <kdrbipul@gmail.com>
2023-10-05 19:57:55 +0600

    last commit

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -2,9 +2,114 @@ import React from 'react';
 
 const Project = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <section className="py-6 bg-gray-800 text-gray-100">
+	<div className="container p-4 mx-auto space-y-16 sm:p-10">
+		<div className="space-y-4 flex flex-col justify-center items-center">
+			<h3 className="text-2xl font-bold leadi sm:text-5xl">Meet our team</h3>
+			<p className="max-w-2xl text-white">At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur quam natus quis nihil quod, hic explicabo doloribus magnam neque, exercitationem eius sunt!</p>
+		</div>
+		<div className="grid w-full grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-4">
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?0" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?1" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?2" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?3" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?4" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?5" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?6" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+					</div>
+				</div>
+			</div>
+			<div className="space-y-4">
+				<img alt="" className="object-cover h-56 mx-auto mb-4 bg-center rounded-sm bg-gray-500" src="https://source.unsplash.com/240x320/?portrait?7" />
+				<div className="flex flex-col items-center">
+					<h4 className="text-xl font-semibold">Leroy Jenkins</h4>
+					<p className="text-sm text-gray-400">Web developer</p>
+					<div className="flex mt-2 space-x-2">
+						
+						
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+                
+        </>
     );
 };
 
