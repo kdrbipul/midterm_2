6b7d38dae598b797edb646295a935c1169a259fe
kdrbipul <kdrbipul@gmail.com>
2023-10-07 02:15:37 +0600

    authentication complete

--- src/UserContext/ContextApi.jsx
+++ src/UserContext/ContextApi.jsx
@@ -1,10 +1,13 @@
 import React, { createContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, GithubAuthProvider, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 import app from './../firebase/firebase.config';
 
 
 
 const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 
 export const AuthContext = createContext()
@@ -15,15 +18,32 @@ const ContextApi = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const 
+    const googleLogin = () =>{
+        return signInWithPopup(auth,googleProvider)
+    }
+
+    const gitHubLogin = () => {
+        return signInWithPopup(auth,githubProvider)
+    }
 
     const signInUser = (email, password) =>{
         return signInWithEmailAndPassword(auth, email, password)
 
     }
+    // const emailVerify = () =>{
+    //     return sendEmailVerification(auth,currentUser)
+    // }
+
+    const forgatePassword = (userEmail) => {
+        return sendPasswordResetEmail(auth, userEmail)
+    }
 
     const authInfo = {
         createUser,
+        googleLogin,
+        gitHubLogin,
+        // emailVerify,
+        forgatePassword,
         signInUser,
     }
 
