83ac373aef03a8f93ccae0874e39f4e2f48565c1
kdrbipul <kdrbipul@gmail.com>
2023-10-07 06:18:51 +0600

    first commit

--- src/Nothing/Nothing.jsx
+++ src/Nothing/Nothing.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Nothing = () => {
     return (
-        <section className="flex items-center h-full p-16 bg-gray-900 text-gray-100">
+        <section className="container mx-auto my-32 flex items-center justify-center h-full p-16 bg-gray-900 text-gray-100">
 	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
 		<div className="max-w-md text-center">
-			<h2 className="mb-8 font-extrabold text-9xl text-gray-600">
+			<h2 className="mb-8 font-extrabold text-9xl text-yellow-400	">
 				<span className="sr-only">Error</span>404
 			</h2>
 			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
