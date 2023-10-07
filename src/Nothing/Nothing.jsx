11337acd0f51e686554c22a1e0cf988a114fc918
kdrbipul <kdrbipul@gmail.com>
2023-10-07 06:13:08 +0600

    some correction

--- src/Nothing/Nothing.jsx
+++ src/Nothing/Nothing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nothing = () => {
     return (
@@ -10,7 +11,7 @@ const Nothing = () => {
 			</h2>
 			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
 			<p className="mt-4 mb-8 text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-			<a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded bg-violet-400 text-gray-900">Back to homepage</a>
+			<Link to='/'><button rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded bg-violet-400 hover:bg-violet-500 transition-all text-gray-900">Back to homepage</button></Link>
 		</div>
 	</div>
 </section>
